5c96a917d5cb5680a1ddda03213eff7c06d284d9
CRYang369 <01165511@pw.edu.pl>
2023-02-10 00:35:30 +0100

    Updated

--- Throw the dice.py
+++ Throw the dice.py
@@ -21,4 +21,4 @@ while roll_again=='yes' or roll_again=='y':
     print(random.randint(min_val,max_val))
     #Continue?
     roll_again=input("Do you want to continue rolling the dice?(if yes,imput the yes or y)")
-   
+print('Game over')   
